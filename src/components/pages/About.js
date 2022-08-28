490cca5ec5d32e450f3a3542fb337ccf0b97be19
kalliehuynh <kallie1@ualberta.ca>
2022-08-27 22:32:34 -0600

    final touches!

--- src/components/pages/About.js
+++ src/components/pages/About.js
@@ -1,21 +1,38 @@
 import './About.css'
 import DocumentTitle from 'react-document-title'
-import { useEffect } from 'react'
 
 const About = () => {
     return (
         <DocumentTitle title='Accessibility and Inclusivity in Medicine Club'>
             <div className="main">
-                <h1 className='header'>About Us</h1>
+                <h1 className='header' tabIndex={0}>About Us</h1>
                 <div className="about-desc">
                     <p>
-                    Located on Treaty 6 Territory, the <strong>Accessibility and Inclusivity in Medicine (AIM) Club</strong> is a student initiative started in 2021-2022 by <b>Surina Grover</b> and <b>Vienna Bucholz</b> at the University of Alberta’s Faculty of Medicine and Dentistry. 
+                        Located on Treaty 6 Territory, the <strong>Accessibility and Inclusivity in Medicine (AIM) Club</strong> is a student initiative started in 2021-2022 by <b>Surina Grover</b> and <b>Vienna Bucholz</b> at the University of Alberta’s Faculty of Medicine and Dentistry. 
                     </p>
                     <p>
                         We are medical students who are passionate about learning from disability communities and educating medical students in order to better serve our future patients identifying with disability, Deaf communities, Neurodivergence, and beyond. 
                     </p>
                 </div>
-                <div className="advocacy-curriculum">
+                <h1 className='header' tabIndex={0}>Purpose</h1>
+                <div className="about-desc">
+                    <p>
+                        The purpose of the AIM Club is to <strong>increase medical students’ awareness, empathy, and knowledge for those who experience disability, are neurodiverse, are Deaf, and more</strong>. 
+                    </p>
+                    <p>
+                    We do this through club events where we bring in disability knowledge holders and host workshops, we also work on curriculum with UME staff to increase disability representation, and we will be sitting on the 2022-2023 year’s Social Accountability Council (SAC) with the MD Program at the University of Alberta. 
+                    </p>
+                </div>
+                <h1 className='header' tabIndex={0}>Initiatives</h1>
+                <div className="about-desc">
+                    <p>
+                        This club is focused on clinical skill development, in order to <strong>address the lack of curricular emphasis on those experiencing disability, are Deaf, or are neurodiverse</strong>. 
+                    </p>
+                    <p>
+                        In the past we have hosted lunch talks with those from the aforementioned communities, and speaker panels that include interdisciplinary health care professionals, educators, patients, and families. We have also written a curriculum for the Foundations block at the University of Alberta undergraduate medical school (story of the week and Discovery Learning). 
+                    </p>
+                </div>
+                <div className="advocacy-curriculum" tabIndex={0}>
                     <div className="advocacy-curriculum-box">
                         <h2 className='header'>Advocacy</h2>
                         <p>We are advocating for:</p>
--- src/components/pages/Team.js
+++ src/components/pages/Team.js
@@ -2,15 +2,30 @@ import DocumentTitle from "react-document-title"
 import './Team.css'
 
 const Person = ({person}) => {
-    const {name, pronouns, desc, contact, img, alt_text} = person
+    const {role, name, pronouns, desc, contact, img, alt_text} = person
     const img_src = `${process.env.PUBLIC_URL}/images/${img}`
     const mailto = `mailto:${contact}`
-    return (
-        <li key={name} className="person">
+    if (img === '') {
+        const img_src = `${process.env.PUBLIC_URL}/images/${'placeholder.jpeg'}`
+        const alt_text = 'A placeholder image for a person'
+        return (
+            <li key={name} className="person" tabIndex={0}>
             <img src={img_src} alt={alt_text} className='person-image' />
             <div className="person-bio-wrapper">
-                <h3 className="person-name">{name}</h3>
-                <p className="person-pronouns">{pronouns}</p>
+                <h3 className="person-name">{name}, {pronouns}</h3>
+                <p className="person-role">{role}</p>
+                <p className="person-desc">{desc}</p>
+                <p className="person-contact">Contact: <a href={mailto} className="person-contact-email">{contact}</a></p>    
+            </div>
+        </li>
+        )
+    }
+    return (
+        <li key={name} className="person" tabIndex={0}>
+            <img src={img_src} alt={alt_text} className='person-image' />
+            <div className="person-bio-wrapper">
+                <h3 className="person-name">{name}, {pronouns}</h3>
+                <p className="person-role">{role}</p>
                 <p className="person-desc">{desc}</p>
                 <p className="person-contact">Contact: <a href={mailto} className="person-contact-email">{contact}</a></p>    
             </div>
@@ -24,12 +39,31 @@ const Person = ({person}) => {
 const Team = () => {
     const people = [
         {
+            'role': 'Co-Lead',
+            'name': 'Kirsten Webster',
+            'pronouns': 'she/her',
+            'desc': 'Kirsten Webster is a second year Métis medical student at the University of Alberta with ADHD, social anxiety disorder, and fibromyalgia. She was a committee member for the AIM Club in the 2021-2022 year and is returning as a Co-Lead for the 2022-2023 year. Kirsten studied Immunology and Infection in undergrad and is passionate about chronic pain conditions and disabilities, and where they intersect. Feel free to connect with Kirsten through email.',
+            'contact': 'kawebste@ualberta.ca',
+            'img': 'kirsten_webster.jpg',
+            'alt_text': 'Portrait pisture of Kirsten'
+        },
+        {
+            'role': 'Co-Lead',
+            'name': 'Jenice Ma',
+            'pronouns': 'she/her',
+            'desc': 'Jenice Ma (she/her) is a nondisabled second year medical student at the Unversity of Alberta, on Treaty 6 territory. She was a committee member for the Accessibility and Inclusivity in Medicine AIM Club in her first year and is incredibly excited to be returning as a Co-Lead this year. She is passionate about the club’s values and goals and is excited to share her passion with others in the medical field. Reach out to Jenice anytime through email!',
+            'contact': 'jnma@ualberta.ca',
+            'img': '',
+            'alt_text': ''
+        },
+        {
+            'role': 'Club Advisor and Coordinator',
             'name': 'An Nguyen',
             'pronouns': 'she/her',
             'desc': 'An Nguyen is a nondisabled Vietnamese settler and second year medical student at the University of Alberta, on Treaty 6 territory. She was a Co-Lead for the Accessibility and Inclusivity in Medicine (AIM) Club in the 2021-2022 year and is returning as a Club Advisor and Coordinator for the 2022-2023 year. An had the pleasure of studying Critical Disability Studies during graduate school, earning her Master of Arts. She loves working with AIM and is excited for what’s to come this year. Feel free to connect with An through email!',
             'contact': 'huean@ualberta.ca',
             'img': 'an_nguyen.jpg',
-            'alt_text': 'portrait of an'
+            'alt_text': 'Portrait pisture of An'
         }
     ]
 
